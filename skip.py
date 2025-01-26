33be05469a1528ca97e11b7c3db22273e7846c4a
Ernesto Elsäßer <ernesto.elsaesser@gmail.com>
2025-01-26 11:22:09 +0100

    Refactor to allow rearrangement

--- skip.py
+++ skip.py
@@ -4,14 +4,15 @@ import hashlib
 import protobuf_header
 
 
-# usage: python3 skip.py INPUT_PATH OUTPUT_PATH SKIPPED_CHAPTERS
+# usage: python3 skip.py INPUT_PATH OUTPUT_DIR CHAPTER_LIST
 # INPUT_PATH - path to a 500304E0 file (Tonie Audio Format) from the SD card
-# OUTPUT_PATH - path to the output file which then can be copied to the SD card
-# SKIPPED_CHAPTERS - comma-separated list of chapter numbers (starting from 1)
+# OUTPUT_DIR - path to the output folder
+# CHAPTER_LIST - comma-separated list of chapter numbers (starting from 1)
 
 # partially adapted from https://github.com/bailli/opus2tonie
 
 
+OGG_MAGIC = b"OggS"
 PAGE_HEADER_FORMAT = "<BBQLLLB"
 SAMPLE_RATE_KHZ = 48
 FRAME_DURATIONS = {c: [2.5, 5, 10, 20][c % 4] * SAMPLE_RATE_KHZ
@@ -34,12 +35,22 @@ def crc32(bytestream):
     return crc
 
 
-input_path, output_path, skip_list = sys.argv[1:4]
+def adjust_page_header(header, body, granule_position, page_num):
+    modified_header = list(header)
+    modified_header[2] = granule_position
+    modified_header[4] = page_num
+    modified_header[5] = 0  # zero checksum
+    header_data = struct.pack(PAGE_HEADER_FORMAT, *modified_header)
+    checksum = crc32(OGG_MAGIC + header_data + body)
+    modified_header[5] = checksum
+    return struct.pack(PAGE_HEADER_FORMAT, *modified_header)
+
+
+input_path, output_dir, chapter_list = sys.argv[1:4]
+
+output_chapter_nums = {int(n) for n in chapter_list.split(",")}
 
 input_file = open(input_path, "rb")
-output_file = open(output_path, "wb")
-skipped_chapters = {int(n) for n in skip_list.split(",")}
-
 file_size = input_file.seek(0, 2)
 input_file.seek(0)
 
@@ -47,40 +58,36 @@ header_size = struct.unpack(">L", input_file.read(4))[0]
 header_data = input_file.read(header_size)
 orig_header = protobuf_header.TonieHeader.FromString(header_data)
 
-chapter_nums = {n: i+1 for i, n in enumerate(orig_header.chapterPages)}
+page_chapter_nums = {n: i+1 for i, n in enumerate(orig_header.chapterPages)}
 
-output_file.write(bytearray(0x1000)) # placeholder
-
-sha1 = hashlib.sha1()
-chapter_pages = []
-granule_position = 0
-next_page_num = 0
-skip = False
+prefix_page_data = b""
+chapter_pages = {}
+current_chapter_num = None
+current_chapter_pages = None
 
 while input_file.tell() < file_size:
 
     # https://datatracker.ietf.org/doc/html/rfc3533#section-6
-    magic = input_file.read(4)
-    assert magic == b"OggS", magic
+    assert input_file.read(4) == OGG_MAGIC
     header_data = input_file.read(23)
     page_header = struct.unpack(PAGE_HEADER_FORMAT, header_data)
 
     page_num = page_header[4]
-    chapter_num = chapter_nums.get(page_num)
+    chapter_num = page_chapter_nums.get(page_num)
     if chapter_num is not None:
-        skip = chapter_num in skipped_chapters
-        print("chapter", chapter_num, "-", "skip" if skip else "copy")
-        if not skip:
-            chapter_pages.append(next_page_num)
+        if current_chapter_num is not None:
+            chapter_pages[current_chapter_num] = current_chapter_pages
+        current_chapter_num = chapter_num
+        current_chapter_pages = []
 
     packet_count = page_header[6]
     packet_table = input_file.read(packet_count)
-    packet_data = b""
+    body_data = packet_table
     duration = 0
     continued = False
     for length in packet_table:
         packet = input_file.read(length)
-        packet_data += packet
+        body_data += packet
 
         if page_num > 1 and not continued:
             info = struct.unpack("<B", packet[0:1])[0]
@@ -98,32 +105,71 @@ while input_file.tell() < file_size:
 
         continued = length == 255
 
-    # need page 2 for alignment
-    if page_num > 2:
-        if skip:
-            continue
-        granule_position += duration
-        modified_header = list(page_header)
-        modified_header[2] = granule_position
-        modified_header[4] = next_page_num
-        modified_header[5] = 0  # zero checksum
-        header_data = struct.pack(PAGE_HEADER_FORMAT, *modified_header)
-        checksum = crc32(magic + header_data + packet_table + packet_data)
-        modified_header[5] = checksum
-        header_data = struct.pack(PAGE_HEADER_FORMAT, *modified_header)
+    if page_num < 2:
+        prefix_page_data += header_data + body_data
+    else:
+        current_chapter_pages.append((header_data, body_data, duration))
 
-    page_data = magic + header_data + packet_table + packet_data
+chapter_pages[current_chapter_num] = current_chapter_pages
+input_file.close()
+
+
+for chapter_num, pages in chapter_pages.items():
+
+    output_file = open(output_dir + f"/chapter{chapter_num}.ogg", "wb")
+    output_file.write(prefix_page_data)
+    granule_position = 0
+    next_page_num = 2
+
+    for header_data, body_data, duration in pages:
+        granule_position += duration
+        mod_header_data = adjust_page_header(header_data, body_data,
+                                         granule_position, next_page_num)
+        page_data = OGG_MAGIC + mod_header_data + body_data
+        output_file.write(page_data)
+        next_page_num += 1
+
+
+output_file = open(output_dir + "/500304E0", "wb")
+output_file.write(bytearray(0x1000)) # placeholder
+
+sha1 = hashlib.sha1()
+output_file.write(prefix_page_data)
+sha1.update(prefix_page_data)
+
+output_chapter_page_nums = []
+granule_position = 0
+next_page_num = 2
+
+# need third page for block alignment
+if output_chapter_nums[0] != 1:
+    header_data, body_data, duration = chapter_pages[1]
+    page_data = OGG_MAGIC + header_data + body_data
     sha1.update(page_data)
     output_file.write(page_data)
+    granule_position += duration
     next_page_num += 1
 
-input_file.close()
+
+for chapter_num in output_chapter_nums:
+
+    output_chapter_page_nums.append(next_page_num)
+
+    for header_data, body_data, duration in chapter_pages[chapter_num]:
+        granule_position += duration
+        mod_header_data = adjust_page_header(page_header, body_data,
+                                            granule_position, next_page_num)
+        page_data = OGG_MAGIC + mod_header_data + body_data
+        sha1.update(page_data)
+        output_file.write(page_data)
+        next_page_num += 1
+
 
 fixed_header = protobuf_header.TonieHeader()
 fixed_header.dataHash = sha1.digest()
 fixed_header.dataLength = output_file.seek(0, 1) - 0x1000
 fixed_header.timestamp = orig_header.timestamp
-fixed_header.chapterPages.extend(chapter_pages)
+fixed_header.chapterPages.extend(output_chapter_nums)
 fixed_header.padding = bytes(0x100)
 
 fixed_header_data = fixed_header.SerializeToString()
