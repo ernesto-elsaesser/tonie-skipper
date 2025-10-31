089e0dc88d65e77d3f721712e7d555222d726157
Ernesto Elsäßer <ernesto.elsaesser@gmail.com>
2025-10-31 19:54:47 +0100

    Improve script args

--- tonie/__init__.py
+++ tonie/__init__.py
@@ -5,15 +5,14 @@ def export():
     """
     Export tonie chapters as Ogg files
 
-    INPUT_DIR - path to folder from SD card
-    OUTPUT_DIR - path to the output folder
+    input_path - path to the input tonie audio file
+    output_dir - path to the output tonie audio file
     """
 
-    input_dir, output_dir = sys.argv[1:3]
+    input_path, output_dir = sys.argv[1:3]
 
-    in_file_name = f"{input_dir}/500304E0"
-    print(in_file_name)
-    with open(in_file_name, "rb") as in_file:
+    print(input_path)
+    with open(input_path, "rb") as in_file:
         tonie_audio = audio.parse_tonie(in_file)
 
     for chapter_num in tonie_audio.header.chapter_start_pages:
@@ -26,22 +25,20 @@ def skip():
     """
     Remove chapter of an existing tonie
 
-    INPUT_DIR - path to folder from SD card
-    OUTPUT_DIR - path to the output folder
-    CHAPTER_LIST - comma-separated list of chapter numbers (starting from 0)
+    input_path - path to the input tonie audio file
+    output_path - path to the output folder
+    chapter_list - comma-separated list of chapter numbers (starting from 0)
     """
 
-    input_dir, output_dir, chapter_list = sys.argv[1:4]
+    input_path, output_path, chapter_list = sys.argv[1:4]
     output_chapter_nums = [int(n) for n in chapter_list.split(",")]
 
-    in_file_name = f"{input_dir}/500304E0"
-    print(in_file_name)
-    with open(in_file_name, "rb") as in_file:
+    print(input_path)
+    with open(input_path, "rb") as in_file:
         tonie_audio = audio.parse_tonie(in_file)
 
-    out_file_name = f"{output_dir}/500304E0"
-    print(out_file_name)
-    with open(out_file_name, "wb") as out_file:
+    print(output_path)
+    with open(output_path, "wb") as out_file:
         audio.compose(tonie_audio, out_file, output_chapter_nums)
 
 
@@ -49,16 +46,15 @@ def swap():
     """
     Replace all chapters of an existing tonie with a list of own Ogg files
 
-    INPUT_DIR - path to folder from SD card
-    OUTPUT_DIR - path to the output folder
-    OPUS_PATH - path to Ogg Opus file (one or more)
+    input_path - path to the input tonie audio file
+    output_path - path to the output tonie audio file
+    *opus_path - path to Ogg Opus file (one or more)
     """
 
-    input_dir, output_dir, *opus_paths = sys.argv[1:]
+    input_path, output_path, *opus_paths = sys.argv[1:]
 
-    in_file_name = f"{input_dir}/500304E0"
-    print(in_file_name)
-    with open(in_file_name, "rb") as in_file:
+    print(input_path)
+    with open(input_path, "rb") as in_file:
         tonie_audio = audio.parse_tonie(in_file)
 
     chapter_nums = []
@@ -68,8 +64,7 @@ def swap():
             chapter_num = audio.append_chapter(tonie_audio, opus_file)
             chapter_nums.append(chapter_num)
 
-    out_file_name = f"{output_dir}/500304E0"
-    print(out_file_name)
-    with open(out_file_name, "wb") as out_file:
+    print(output_path)
+    with open(output_path, "wb") as out_file:
         audio.compose(tonie_audio, out_file, chapter_nums)
 
